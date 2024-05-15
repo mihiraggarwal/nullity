35b99186ad8128f011c63def204f8c58eae3af25
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-05-16 02:23:01 +0530

    structure

---
+++ lin_alg/__init__.py
@@ -0,0 +1,1 @@
+from lin_alg.matrix import Matrix
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package for all things linear algebra'
+
+setup(
+    name="lin_alg",
+    version=VERSION,
+    author="Mihir Aggarwal",
+    author_email="mail@mihiraggarwal.me",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['linear', 'algebra', 'linear algebra', 'linalg', 'lin-alg', 'linearalgebra', 'eigen', 'eigenvalue', 'eigenvector'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: GPL-3.0 License"
+    ]
+)
