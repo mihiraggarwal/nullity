dd71d98c3b001c33ad6aad50941cebd76f0effe6
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-05-16 05:04:22 +0530

    setup changes

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
 DESCRIPTION = 'A package for all things linear algebra'
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md")) as f:
+    long_description = f.read()
 
 setup(
     name="lin_alg",
@@ -9,12 +14,13 @@ setup(
     author="Mihir Aggarwal",
     author_email="mail@mihiraggarwal.me",
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=['numpy'],
     keywords=['linear', 'algebra', 'linear algebra', 'linalg', 'lin-alg', 'linearalgebra', 'eigen', 'eigenvalue', 'eigenvector'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-        "License :: GPL-3.0 License"
     ]
 )
